eb18488689cc31a4a2bdd78123d689eaed6f5d23
luthien716 <luthien716@hotmail.com>
2017-07-13 17:33:36 +0800

    Homework01-Pokemon類別

---
+++ PokemonLibrary/PokemonLibrary/Pokemon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonLibrary
+{
+    public class Pokemon
+    {
+        public string Name;
+        public string Sex;
+        public string HP;
+        public string Weight;
+        public string Type;
+        public string Height;
+        public string PlayerStardust;
+        public string PlayerCandy;
+        public string PowerUp;
+        public string Evolve;
+        public string Skills;
+        public string LocationCaught;
+        public string DateCaught;
+    }
+}
---
+++ Pokemon_Homework/Pokemon/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokemonLibrary;
+
+namespace PokemonList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon apokemon = new Pokemon()
+            {
+                Name = "妙蛙種子",
+                Sex = "♂",
+                HP = "60 / 60",
+                Weight = "5.94",
+                Type = "草 / 毒",
+                Height = "0.67",
+                PlayerStardust = "271484",
+                PlayerCandy = "27",
+                PowerUp = "3000 星星沙子   3 顆妙蛙種子的糖果",
+                Evolve = "25 顆妙蛙種子的糖果",
+                Skills = "藤鞭、汙泥炸彈",
+                LocationCaught = "台灣 新北 三重區",
+                DateCaught = "09/14/2016",
+
+            };
+            Console.WriteLine("Pokemon－{0} {1}\nHP {2}\n體重：{3}Kg  類型：{4}　身高：{5}Cm\n\n" +
+                "目前擁有的星星沙子：{6}  目前擁有的妙蛙種子糖果：{7}\n\n" +
+                "強化所需的材料：{8}\n所需的材料：{9}\n技能：{10}\n\n" +
+                "捕捉地點：{11}\n捕捉時間：{12}\n\n"
+                , apokemon.Name
+                , apokemon.Sex
+                , apokemon.HP
+                , apokemon.Weight
+                , apokemon.Type
+                , apokemon.Height
+                , apokemon.PlayerStardust
+                , apokemon.PlayerCandy
+                , apokemon.PowerUp
+                , apokemon.Evolve
+                , apokemon.Skills
+                , apokemon.LocationCaught
+                , apokemon.DateCaught
+                );
+
+        }
+    }
+}
